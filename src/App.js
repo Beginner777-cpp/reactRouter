135e676a383af2bb0de688a5dcca887502ab0ea7
jasurbek <jasurbekjumaboyev57@gmail.com>
2021-08-23 20:29:24 +0500

    added router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -10,10 +11,17 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    
     return (
       <div>
         <NavBar />
+        <Switch>
+          
+          <Route path="/products" component={Products} />
+          <Route path="/posts" component={Posts} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/productDetails" component={ProductDetails} />
+          <Route path="/" component={Home} />
+        </Switch>
       </div>
     );
   }
